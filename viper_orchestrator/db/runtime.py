56e05dc2041103e5306dd03c844b431747b334b4
michael <mstclair@millionconcepts.com>
2023-09-26 15:47:20 -0400

    cleanup

--- viper_orchestrator/db/runtime.py
+++ viper_orchestrator/db/runtime.py
@@ -1,18 +1,24 @@
-"""orchestrator-specific database configuration"""
+"""
+'singleton' module for setting up or connecting to orchestrator db. 
+should be imported if and only if you wish to execute the database
+initialization and connection workflow. This module's ENGINE member is
+intended for use as a database connection.
+"""
 import atexit
 import shutil
 import time
 from functools import partial
 from pathlib import Path
 
-from hostess.subutils import Viewer, run
+from hostess.subutils import Viewer
 from sqlalchemy import create_engine, select, insert
 from sqlalchemy.orm import Session
 
-from viper_orchestrator.db.config import BASES, TEST_DB_PATH, TEST
+from viper_orchestrator.db.config import BASES, TEST, TEST_DB_PATH
 from vipersci.vis.db.image_tags import ImageTag, taglist
 
-INITIALIZED_DB = False
+# lists defining managed processes that we will, respectively, clean up and
+# not clean up  on interpreter exit
 KILL_TARGETS = []
 DO_NOT_KILL = []
 
@@ -30,71 +36,86 @@ def listkiller(processes: list[Viewer]):
 
 atexit.register(partial(listkiller, KILL_TARGETS))
 
+
+class PostgresInitError(OSError):
+    pass
+
+
+# coordinate system center
+LAT_0, LON_0 = -85.42088, 31.6218
+# canonical lunar radius
+LUNAR_RADIUS = 1737400
+# commands to initialize new postgres server
+SPATIAL_REF_VALUES = f"( 910101, 'ROVER', 910101, '+proj=stere +lat_0={LAT_0} +lon_0={LON_0} +R={LUNAR_RADIUS} +units=m')"
+INIT_COMMANDS = (
+    f"initdb -D {TEST_DB_PATH}",
+    f"postgres -D {TEST_DB_PATH}",
+    f'psql -d {TEST_DB_PATH.name} -c "CREATE EXTENSION postgis"',
+    f"""psql -d {TEST_DB_PATH.name} -c "INSERT into spatial_ref_sys (srid, auth_name, auth_srid, proj4text) values {SPATIAL_REF_VALUES}\"""",
+)
+
+
+def construct_postgres_error(viewer, text="postgres server failed init"):
+    return PostgresInitError(
+        f"{text}\ncommand {viewer.command} failed:\n\n{','.join(viewer.err)}"
+    )
+
+
+def kill_and_delete(viewer):
+    viewer.kill()
+    print('removing db path')
+    shutil.rmtree(TEST_DB_PATH)
+
+
 if TEST is True:
+    # initialize the test database if it doesn't exist
     if not Path(TEST_DB_PATH).exists():
-        INITIALIZED_DB = True
-        for cmd in (
-            f"initdb -D {TEST_DB_PATH}",
-            f"postgres -D {TEST_DB_PATH}",
-            f'psql -d {TEST_DB_PATH.name} -c "CREATE EXTENSION postgis"',
-            f"""psql -d {TEST_DB_PATH.name} -c "INSERT into spatial_ref_sys (srid, auth_name, auth_srid, proj4text) values ( 910101, 'VIPER', 910101, '+proj=stere +lat_0=-85.42088 +lon_0=31.6218 +R=1737400 +units=m')\"""",
-        ):
+        for cmd in INIT_COMMANDS:
             pginit = Viewer.from_command(cmd)
             if cmd.startswith("postgres -D"):
-                POSTGRES_PID = pginit.pid
-                time.sleep(1)
-                if (
-                    "database system is ready to accept connections" not
-                    in pginit.err[-1]
-                ):
-                    print('removing db path')
-                    shutil.rmtree(TEST_DB_PATH)
-                    pginit.kill()
-                    raise OSError(
-                        f"postgres server process failed to launch:\n\n"
-                        f"{','.join(pginit.err)}"
+                time.sleep(1)  # TODO: replace with explicit connection check
+                if "system is ready to accept connections" not in pginit.err[-1]:
+                    kill_and_delete(pginit)
+                    raise construct_postgres_error(
+                        pginit, "server failed launch"
                     )
                 KILL_TARGETS.append(pginit)
             else:
                 pginit.wait()
                 if pginit.returncode() != 0:
-                    print('removing db path')
-                    shutil.rmtree(TEST_DB_PATH)
-                    raise OSError(
-                        f"postgres database failed to initialize\ncommand "
-                        f"{pginit.command} failed:\n\n{','.join(pginit.err)}"
-                    )
+                    kill_and_delete(pginit)
+                    raise construct_postgres_error(pginit)
+    # otherwise, simply connect to the test database
     else:
         pginit = Viewer.from_command(f"postgres -D {TEST_DB_PATH}")
         time.sleep(1)
-        if (
-            "database system is ready to accept connections" not
-            in pginit.err[-1]
-        ):
+        if "system is ready to accept connections" not in pginit.err[-1]:
             # if it fails with the "another server might be running" message,
             # assume we already launched it on purpose. i.e., don't kill it
             # when we leave...
             if "another server might be running" not in pginit.stderr[0]:
                 pginit.kill()
-                raise OSError(
-                    f"postgres server process failed to launch:\n"
-                    f"{','.join(pginit.err)}"
-                )
+                raise construct_postgres_error(pginit, 'server failed launch')
             # ...but make sure we can still access it if we like.
             DO_NOT_KILL.append(pginit)
         else:
             KILL_TARGETS.append(pginit)
     ENGINE = create_engine(f"postgresql:///{TEST_DB_PATH.name}")
 else:
-    raise NotImplementedError  # system-level postgresql server
+    # code for connecting to the system-level postgresql server, whose
+    # parameters are not yet defined, goes here
+    raise NotImplementedError
 
 # TODO: this should be harmless, make sure it is
 for base in BASES:
     base.metadata.create_all(ENGINE)
 
 
+# initialize pseudo-enum from configuration file
+# NOTE: semi-vendored from init functions in science repo. it must exactly copy 
+# this 'official' code and should not be changed.
+# TODO: add license note
 def set_up_tags():
-    """semi-vendored from vipersci"""
     with Session(ENGINE) as session:
         # Establish image_tags
         scalars = session.scalars(select(ImageTag))
@@ -120,44 +141,3 @@ def set_up_tags():
 
 
 set_up_tags()
-
-
-# TODO: following is probably cruft now
-# def sqlite_monkeypatch_rawproduct():
-#     """
-#     monkeypatch RawProduct to cause its as_dict method to return tz-aware
-#     datetimes. intended for testing with sqlite (SQLAlchemy's sqlite backend
-#     does not store datetimes as tz-aware even if you ask it to).
-#
-#     TODO: copied from higher level pending module organization
-#     """
-#     import datetime as dt
-#
-#     import vipersci.vis.db.image_records
-#     from vipersci.pds.datetime import isozformat
-#
-#     def asdict(self):
-#         d = {}
-#
-#         for c in self.__table__.columns:
-#             if isinstance(getattr(self, c.name), dt.datetime):
-#                 date_time = getattr(self, c.name).replace(
-#                     tzinfo=dt.timezone.utc
-#                 )
-#                 d[c.name] = isozformat(date_time)
-#             else:
-#                 d[c.name] = getattr(self, c.name)
-#
-#         # the SQLAlchemy metaclass constructor does not consistently call the
-#         # class's __init__ method on db retrieval, so only attempt to merge
-#         # labelmeta if it actually exists
-#         if "labelmeta" in dir(self):
-#             d.update(self.labelmeta)
-#
-#         return d
-#
-#     vipersci.vis.db.image_records.ImageRecord.asdict = asdict
-#
-#
-# if TEST is True:
-#     sqlite_monkeypatch_rawproduct()
